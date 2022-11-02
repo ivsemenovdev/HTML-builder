b1cc924fa03f3bab9597f46b1456cc270d00a635
Ivan Semenov <ivsemenovdev@gmail.com>
2022-11-02 23:39:53 +0300

    feat: read a file with content output to the console HTML-builder/01-read-file/

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   'env': {
     'commonjs': true,
-    'es2022': true,
+    // 'es2022': false,
     'node': true
   },
   'extends': 'eslint:recommended',
---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourcePath = path.join(__dirname, 'text.txt');
+
+const stream = new fs.ReadStream(sourcePath, {encoding: 'utf-8'});
+
+stream.on('readable', function () {
+  const data = stream.read();
+  if (data) console.log(data);
+});
