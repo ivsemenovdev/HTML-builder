25e9c9a5a383523a2a2f9182794a8a76af042a40
Ivan Semenov <ivsemenovdev@gmail.com>
2022-11-07 12:32:25 +0300

    feat: copy folder HTML-builder/04-copy-directory/

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
 
+const sourcePath = path.join(__dirname, 'files');
+const destinationPath = path.join(__dirname, 'files-copy');
+
+fsPromises.mkdir(destinationPath).then(function () {
+  console.log('Directory created successfully');
+}).catch(function () {
+  console.log('Directory already exist, all files was overwritten');
+});
+
+fs.readdir(sourcePath,
+  {withFileTypes: true},
+  (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+
+        fsPromises.copyFile(path.join(sourcePath, file.name), path.join(destinationPath, file.name))
+          .then(function () {
+            console.log(`File ${file.name} copied`);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+      });
+    }
+  });
