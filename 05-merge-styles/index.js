cd4346f16baf30214901b2f5728c3590f12cfb32
Ivan Semenov <ivsemenovdev@gmail.com>
2022-11-07 14:16:37 +0300

    feat: assemble css bundles HTML-builder/05-merge-styles/

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
+
+const sourcePath = path.join(__dirname, 'styles');
+const destinationPath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+let data = '';
+
+fsPromises.writeFile(
+  destinationPath, data, {
+    encoding: 'utf8',
+    flag: 'w',
+    mode: 0o666
+  })
+  .then(function () {
+    fs.readdir(sourcePath,
+      {withFileTypes: true},
+      (err, files) => {
+        if (err)
+          console.log(err);
+        else {
+
+          files.forEach(file => {
+            const extension = path.extname(file.name);
+            if (extension === '.css') {
+              fsPromises.readFile(path.join(sourcePath, file.name))
+                .then(value => {
+                  fsPromises.appendFile(destinationPath, value)
+                    .then(function () {
+                      console.log(`Styles from ${file.name} added to bundle.css`);
+                    })
+                    .catch(function (error) {
+                      console.log(error);
+                    });
+                });
+            }
+          });
+
+        }
+      });
+    console.log('File written successfully\n');
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+
+
